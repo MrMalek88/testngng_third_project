0ed29d6e76d14c4eda3dc799d21cf3961304b11c
Malek Shaar <malekshaar@gmail.com>
2023-02-07 17:56:40 -0600

    Initial commit

---
+++ src/test/java/scripts/CarvanaBaseTest.java
@@ -0,0 +1,29 @@
+package scripts;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import pages.CarvanaBasePage;
+import utils.ConfigReader;
+import utils.Driver;
+
+public class CarvanaBase {
+
+        CarvanaBasePage carvanaBasePage;
+        CarvanaBase carvanaBase;
+        CarvanaTest carvanaTest;
+
+        WebDriver driver;
+
+    @BeforeMethod
+    public void setup(){
+        driver = Driver.getDriver();
+        driver.get(ConfigReader.getProperty("appURL"));
+        carvanaBasePage = new CarvanaBasePage();
+    }
+
+    @AfterMethod
+    public void teardown (){
+        Driver.quitDriver();
+    }
+}
